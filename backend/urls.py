2403d6b8fbe6d4a9ad6593534620d793698ca011
Chenlan <630473969@qq.com>
2025-08-09 01:24:21 +0800

    fix: add root health check route for render

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from backend.views import db_info_view, gr_info_view
+from django.http import JsonResponse  # ✅ 引入 JsonResponse 响应类
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({'status': 'ok'})),
     path('book/db/', db_info_view),         # /api/book/?q=xxx
     path('book/gr/', gr_info_view),  # /api/book/status/?isbn=xxx
 ]
 
 #python manage.py runserver
+
+
+
+
